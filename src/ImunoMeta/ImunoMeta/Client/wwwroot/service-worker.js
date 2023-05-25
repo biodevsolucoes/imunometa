7c0f8f674be19f5c4d64229ee2db82602dec18fc
Bruno Figueiredo <bruno8f@gmail.com>
2023-05-25 10:46:39 -0300

    Correção do Service Worker

--- src/ImunoMeta/ImunoMeta/Client/wwwroot/service-worker.js
+++ src/ImunoMeta/ImunoMeta/Client/wwwroot/service-worker.js
@@ -1,4 +1,23 @@
 // In development, always fetch from the network and do not enable offline support.
 // This is because caching would make development more difficult (changes would not
 // be reflected on the first load after each change).
+// Caution! Be sure you understand the caveats before publishing an application with
+// offline support. See https://aka.ms/blazor-offline-considerations
+
+self.importScripts('./service-worker-assets.js');
 self.addEventListener('fetch', () => { });
+
+async function onFetch(event) {
+    let cachedResponse = null;
+    if (event.request.method === 'GET') {
+        // For all navigation requests, try to serve index.html from cache
+        // If you need some URLs to be server-rendered, edit the following check to exclude those URLs
+        const shouldServeIndexHtml = event.request.mode === 'navigate'
+            && !event.request.url.includes('/connect/')
+            && !event.request.url.includes('/Identity/');
+
+        const request = shouldServeIndexHtml ? 'index.html' : event.request;
+    }
+
+    return fetch(event.request);
+}
